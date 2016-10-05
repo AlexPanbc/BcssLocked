8b18281004127370cf060567fe43ccfb0c1e5bb6
liuhuichao <2207221755@qq.com>
2016-10-05 15:13:41 +0800

    添加注解-liuhuichao-2016年10月5日15:13:30

--- src/main/java/com/pbc/controller/UserInfoController.java
+++ src/main/java/com/pbc/controller/UserInfoController.java
@@ -5,7 +5,9 @@ import com.pbc.po.UserInfo;
 import com.pbc.service.UserInfoService;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class UserInfoController {
     private static Logger log = LogManager.getLogger(UserInfoController.class);
 
+    @Autowired
     private UserInfoService userInfoService;
 
     /**
@@ -27,7 +30,7 @@ public class UserInfoController {
      * @return
      */
     @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
-    public String get(int id) {
+    public String get(@PathVariable("id") int id) {
         log.debug("根据用户ID查询用户，接口参数为：" + JSON.toJSONString(id));//发布到服务器之后，供调试时候查看log使用
          userInfoService.get(id);
         return "index";
