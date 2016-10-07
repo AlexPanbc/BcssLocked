0d60a675cd98c4d7d789a0398b9d64334d3cdccd
liuhuichao <2207221755@qq.com>
2016-10-07 20:29:02 +0800

    提交相应参数-liuhuichao-2016年10月7日20:28:55

---
+++ src/main/java/com/pbc/webparams/responseparams/GoodsListResponse.java
@@ -0,0 +1,86 @@
+package com.pbc.webparams.responseparams;
+
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+/**
+ *
+ * 功能：查询商品列表返回单个实体数据类型
+ * Created by LiuHuiChao on 2016/10/7.
+ */
+public class GoodsListResponse  implements Serializable {
+
+    private Integer id;
+
+    private String name;
+
+    private String typeName;
+
+    private Float price;
+
+    private Integer number;
+
+    private String createdon;
+
+    private String modifiedon;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getCreatedon() {
+        return createdon;
+    }
+
+    public void setCreatedon(String createdon) {
+        this.createdon = createdon;
+    }
+
+    public String getModifiedon() {
+        return modifiedon;
+    }
+
+    public void setModifiedon(String modifiedon) {
+        this.modifiedon = modifiedon;
+    }
+}
