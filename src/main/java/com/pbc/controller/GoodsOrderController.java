2d86fb158faf8a03682287530c8e43fb179c7a46
AlexPanbc <1020208895pan@gmail.com>
2016-10-09 21:53:52 +0800

    商品类型  http://localhost:8080/BcssLocked/goodsType/getAll 调用这个api返回的数据里需要有 正确的时间格式比如2016-10-08 14:39:50

--- src/main/java/com/pbc/controller/GoodsOrderController.java
+++ src/main/java/com/pbc/controller/GoodsOrderController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
  * Created by Alex on 2016/10/9.
  */
 @Controller
-@RequestMapping("order")
+@RequestMapping("goodsorder")
 public class GoodsOrderController extends BaseController {
     private static Logger log = LogManager.getLogger(GoodsOrderController.class);
 
@@ -21,7 +21,7 @@ public class GoodsOrderController extends BaseController {
     private GoodsOrderService goodsOrderService;
 
     /**
-     * 接口地址示例：http://localhost:8080/BcssLocked/order/getAll
+     * 接口地址示例：http://localhost:8080/BcssLocked/goodsorder/getAll
      * 获取所有订单
      *
      * @return
@@ -33,7 +33,7 @@ public class GoodsOrderController extends BaseController {
     }
 
     /**
-     * 接口地址示例：http://localhost:8080/BcssLocked/order/get
+     * 接口地址示例：http://localhost:8080/BcssLocked/goodsorder/get
      * 根据id查询订单信息
      *
      * @param id
@@ -47,7 +47,7 @@ public class GoodsOrderController extends BaseController {
     }
 
     /**
-     * 接口地址示例：http://localhost:8080/BcssLocked/order/add
+     * 接口地址示例：http://localhost:8080/BcssLocked/goodsorder/add
      * 创建订单
      *
      * @param o
@@ -61,7 +61,7 @@ public class GoodsOrderController extends BaseController {
     }
 
     /**
-     * 接口地址示例：http://localhost:8080/BcssLocked/order/del
+     * 接口地址示例：http://localhost:8080/BcssLocked/goodsorder/del
      * 删除订单信息
      *
      * @param id
