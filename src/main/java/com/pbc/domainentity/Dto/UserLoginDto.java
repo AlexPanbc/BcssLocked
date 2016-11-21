3884bf52a1cb9cc3d921fbda3aa1ec92f8e0bbff
liuhuichao <2207221755@qq.com>
2016-11-21 14:25:43 +0800

    添加用户登录dto-liuhuichao-2016年11月21日14:25:35

---
+++ src/main/java/com/pbc/domainentity/Dto/UserLoginDto.java
@@ -0,0 +1,26 @@
+package com.pbc.domainentity.Dto;
+
+/**
+ * Created by LiuHuiChao on 2016/11/21.
+ */
+public class UserLoginDto {
+    private String name;
+    private String pass;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
