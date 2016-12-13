d4ff323297f979cb25eefbf826a39f8f03d165e9
liuhuichao <2207221755@qq.com>
2016-12-13 16:05:53 +0800

    条件查询封装实体类-liuhuichao-2016年12月13日16:05:49

---
+++ src/test/java/test/HbaseTest/QueryCondition.java
@@ -0,0 +1,55 @@
+package test.HbaseTest;
+
+import org.apache.hadoop.hbase.filter.CompareFilter;
+
+/**
+ *
+ * 条件查询封装实体类
+ * Created by liuhuichao on 2016/12/13.
+ */
+public class QueryCondition {
+
+    String family; //列族
+    String qualifier; //列修饰符
+    CompareFilter.CompareOp  compareOp; //操作符
+    String value; //列值
+
+    public QueryCondition(String family, String qualifier, CompareFilter.CompareOp compareOp, String value) {
+        this.family = family;
+        this.qualifier = qualifier;
+        this.compareOp = compareOp;
+        this.value = value;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getQualifier() {
+        return qualifier;
+    }
+
+    public void setQualifier(String qualifier) {
+        this.qualifier = qualifier;
+    }
+
+    public CompareFilter.CompareOp getCompareOp() {
+        return compareOp;
+    }
+
+    public void setCompareOp(CompareFilter.CompareOp compareOp) {
+        this.compareOp = compareOp;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
