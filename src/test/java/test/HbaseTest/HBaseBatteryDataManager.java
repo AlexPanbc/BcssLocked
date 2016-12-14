fde86568e111de62b0290e93f70bb2b3964a65f1
liuhuichao <2207221755@qq.com>
2016-12-14 17:18:07 +0800

    修改代码-liuhuichao-2016年12月14日17:17:24

--- src/test/java/test/HbaseTest/HBaseBatteryDataManager.java
+++ src/test/java/test/HbaseTest/HBaseBatteryDataManager.java
@@ -23,8 +23,8 @@ public class HBaseBatteryDataManager extends Thread {
         private static final HBaseBatteryDataManager INSTANCE = new HBaseBatteryDataManager();
     }
 
-    private  final String  colunm_family_baseData="baseData";//列族baseData
-    private  final String  colunm_family_extraData="extraData";//列族extraData
+    public  final String  colunm_family_baseData="baseData";//列族baseData
+    public  final String  colunm_family_extraData="extraData";//列族extraData
 
     /**
      * 构造：配置初始化
@@ -45,7 +45,7 @@ public class HBaseBatteryDataManager extends Thread {
         }*/
 
 
-        //单击环境下的config
+        //单机环境下的config
         config= HBaseConfiguration.create();
         config.set("hbase.zookeeper.quorum","lhc-centos");
         try{
@@ -53,11 +53,6 @@ public class HBaseBatteryDataManager extends Thread {
         }catch (IOException e){
             e.printStackTrace();
         }
-
-
-
-
-
 
     }
 
@@ -81,52 +76,22 @@ public class HBaseBatteryDataManager extends Thread {
         if(resBatteryDataHistory==null){
             return;
         }
-        Put put=new Put(Bytes.toBytes(resBatteryDataHistory.getBatteryNo()+System.currentTimeMillis()));
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("battery_no"),Bytes.toBytes(resBatteryDataHistory.getBatteryNo()));//电池编号
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("battery_type"),Bytes.toBytes(resBatteryDataHistory.getBatteryType()));//电池类型
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("voltage_deviation"),Bytes.toBytes(resBatteryDataHistory.getVoltageDeviation()));//压差
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("total_voltage"),Bytes.toBytes(resBatteryDataHistory.getTotalVoltage()));//总电压
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("total_current"),Bytes.toBytes(resBatteryDataHistory.getTotalCurrent()));//总电流
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol1"),Bytes.toBytes(resBatteryDataHistory.getVol1()));//电芯电压1
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol2"),Bytes.toBytes(resBatteryDataHistory.getVol2()));//电芯电压2
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol3"),Bytes.toBytes(resBatteryDataHistory.getVol3()));//电芯电压3
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol4"),Bytes.toBytes(resBatteryDataHistory.getVol4()));//电芯电压4
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol5"),Bytes.toBytes(resBatteryDataHistory.getVol5()));//电芯电压5
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol6"),Bytes.toBytes(resBatteryDataHistory.getVol6()));//电芯电压6
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol7"),Bytes.toBytes(resBatteryDataHistory.getVol7()));//电芯电压7
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol8"),Bytes.toBytes(resBatteryDataHistory.getVol8()));//电芯电压8
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol9"),Bytes.toBytes(resBatteryDataHistory.getVol9()));//电芯电压9
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol10"),Bytes.toBytes(resBatteryDataHistory.getVol10()));//电芯电压10
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol11"),Bytes.toBytes(resBatteryDataHistory.getVol11()));//电芯电压11
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol12"),Bytes.toBytes(resBatteryDataHistory.getVol12()));//电芯电压12
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol13"),Bytes.toBytes(resBatteryDataHistory.getVol13()));//电芯电压13
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol14"),Bytes.toBytes(resBatteryDataHistory.getVol14()));//电芯电压14
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol15"),Bytes.toBytes(resBatteryDataHistory.getVol15()));//电芯电压15
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol16"),Bytes.toBytes(resBatteryDataHistory.getVol16()));//电芯电压16
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol17"),Bytes.toBytes(resBatteryDataHistory.getVol17()));//电芯电压17
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol18"),Bytes.toBytes(resBatteryDataHistory.getVol18()));//电芯电压18
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol19"),Bytes.toBytes(resBatteryDataHistory.getVol19()));//电芯电压19
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("temprature1"),Bytes.toBytes(resBatteryDataHistory.getTemprature1()));//温度1
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("temprature2"),Bytes.toBytes(resBatteryDataHistory.getTemprature2()));//温度2
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("charge_num"),Bytes.toBytes(resBatteryDataHistory.getChargeNum()));//充电次数
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("soc"),Bytes.toBytes(resBatteryDataHistory.getSoc()));//总电压
-        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("create_time"),Bytes.toBytes(resBatteryDataHistory.getCreateTime()));//创建时间
-        if(resBatteryDataHistory.getCityId()!=null){
-            put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("city_id"),Bytes.toBytes(resBatteryDataHistory.getCityId()));//城市ID
-        }
-        if(resBatteryDataHistory.getVehicleId()!=null){
-            put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vehicle_id"),Bytes.toBytes(resBatteryDataHistory.getVehicleId()));//车辆ID
-        }
-        put.addColumn(Bytes.toBytes(colunm_family_extraData),Bytes.toBytes("create_user"),Bytes.toBytes(resBatteryDataHistory.getCreateUser()));//创建人
-        put.addColumn(Bytes.toBytes(colunm_family_extraData),Bytes.toBytes("source"),Bytes.toBytes(resBatteryDataHistory.getSource()));//数据来源
-        put.addColumn(Bytes.toBytes(colunm_family_extraData),Bytes.toBytes("longtitude"),Bytes.toBytes(resBatteryDataHistory.getLongtitude()));//精度
-        put.addColumn(Bytes.toBytes(colunm_family_extraData),Bytes.toBytes("latitude"),Bytes.toBytes(resBatteryDataHistory.getLatitude()));//纬度
-        put.addColumn(Bytes.toBytes(colunm_family_extraData),Bytes.toBytes("remarks"),Bytes.toBytes(resBatteryDataHistory.getRemarks()));//备注
-        put.addColumn(Bytes.toBytes(colunm_family_extraData),Bytes.toBytes("test_user_name"),Bytes.toBytes(resBatteryDataHistory.getTestUserName()));//测试用户
-        table.put(put);
+        table.put(getSavePut(resBatteryDataHistory));
         table.close();
     }
 
+    /**
+     * 批量添加电池数据
+     * @param batteryList
+     */
+    public void addBatteryDataList(List<ResBatteryDataHistory> batteryList) throws IOException {
+        List<Put> puts = new ArrayList<Put>();
+        for(ResBatteryDataHistory battery : batteryList){
+            puts.add(getSavePut(battery));
+        }
+        table.put(puts);
+        table.close();
+    }
 
     /**
      * 根据rowKey获取电池历史数据
@@ -149,12 +114,12 @@ public class HBaseBatteryDataManager extends Thread {
      * @param col
      * @return
      */
-    public  String getCellData(String rowKey,String colFamily,String col) throws IOException {
+    public  byte[] getCellData(String rowKey,String colFamily,String col) throws IOException {
         Get get = new Get(Bytes.toBytes(rowKey));
         get.addColumn(Bytes.toBytes(colFamily),Bytes.toBytes(col));
         //获取的result数据是结果集，还需要格式化输出想要的数据才行
         Result result = table.get(get);
-        String cellValue=new String(result.getValue(colFamily.getBytes(),col==null?null:col.getBytes())).intern();
+        byte[] cellValue= result.getValue(colFamily.getBytes(),col==null?null:col.getBytes());
         table.close();
         return cellValue;
     }
@@ -166,11 +131,10 @@ public class HBaseBatteryDataManager extends Thread {
      * @param columnName
      * @param value
      */
-   public void  updateCell(String rowKey,String familyName, String columnName, String value) throws IOException {
+   public void  updateCell(String rowKey,String familyName, String columnName, byte[] value) throws IOException {
        Put put = new Put(Bytes.toBytes(rowKey));
        put.addColumn(Bytes.toBytes(familyName), Bytes.toBytes(columnName),
-               Bytes.toBytes(value));
-
+               value);
        table.put(put);
        table.close();
    }
@@ -201,10 +165,9 @@ public class HBaseBatteryDataManager extends Thread {
 
     /**
      * 获取所有数据
-     * @param size
      * @return
      */
-    public List<ResBatteryDataHistory> getResultScans(int size) throws IOException {
+    public List<ResBatteryDataHistory> getResultScans() throws IOException {
         Scan scan = new Scan();
         ResultScanner resultScanner = null;
         List<ResBatteryDataHistory> list=new ArrayList<>() ;
@@ -212,6 +175,7 @@ public class HBaseBatteryDataManager extends Thread {
         resultScanner=table.getScanner(scan);
         long endTime=System.currentTimeMillis();
         double spendTime=(endTime-beginTime)/1000.0;
+        System.out.println("扫描表共计时间：------"+spendTime);
         if(resultScanner==null){
             return null;
         }
@@ -240,7 +204,7 @@ public class HBaseBatteryDataManager extends Thread {
         for(QueryCondition query : queryConditionList){
             SingleColumnValueFilter filter = new SingleColumnValueFilter(
                     Bytes.toBytes(query.getFamily()), Bytes.toBytes(query.getQualifier()),
-                    query.getCompareOp(),Bytes.toBytes(query.getValue()));
+                    query.getCompareOp(),query.getValue());
             filter.setFilterIfMissing(true); //设置这些列不存在的数据不返回
             filters.add(filter);
         }
@@ -272,7 +236,7 @@ public class HBaseBatteryDataManager extends Thread {
         for(QueryCondition query : queryConditionList){
             SingleColumnValueFilter filter = new SingleColumnValueFilter(
                     Bytes.toBytes(query.getFamily()), Bytes.toBytes(query.getQualifier()),
-                    query.getCompareOp(),Bytes.toBytes(query.getValue()));
+                    query.getCompareOp(),query.getValue());
             filter.setFilterIfMissing(true);
             filters.add(filter);
         }
@@ -307,7 +271,7 @@ public class HBaseBatteryDataManager extends Thread {
 
             SingleColumnValueFilter filter = new SingleColumnValueFilter(
                     Bytes.toBytes(query.getFamily()), Bytes.toBytes(query.getQualifier()),
-                    query.getCompareOp(),Bytes.toBytes(query.getValue()));
+                    query.getCompareOp(),query.getValue());
             filter.setFilterIfMissing(true);  //去掉没有这种列的数据
 
             if(query.getOperator()!=null){//有操作符的时候
@@ -364,7 +328,7 @@ public class HBaseBatteryDataManager extends Thread {
         for(QueryCondition query : queryConditionList){
             SingleColumnValueFilter filter = new SingleColumnValueFilter(
                     Bytes.toBytes(query.getFamily()), Bytes.toBytes(query.getQualifier()),
-                    query.getCompareOp(),Bytes.toBytes(query.getValue()));
+                    query.getCompareOp(),query.getValue());
             filter.setFilterIfMissing(true);  //去掉没有这种列的数据
 
             if(query.getOperator()!=null){//有操作符的时候
@@ -609,6 +573,56 @@ public class HBaseBatteryDataManager extends Thread {
         return  resBatteryDataHistory;
     }
 
+    /**
+     * 获取插入数据的Put对象
+     * @param resBatteryDataHistory
+     * @return
+     */
+    private Put getSavePut(ResBatteryDataHistory resBatteryDataHistory){
+        Put put=new Put(Bytes.toBytes(resBatteryDataHistory.getBatteryNo()+System.currentTimeMillis()));
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("battery_no"),Bytes.toBytes(resBatteryDataHistory.getBatteryNo()));//电池编号
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("battery_type"),Bytes.toBytes(resBatteryDataHistory.getBatteryType()));//电池类型
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("voltage_deviation"),Bytes.toBytes(resBatteryDataHistory.getVoltageDeviation()));//压差
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("total_voltage"),Bytes.toBytes(resBatteryDataHistory.getTotalVoltage()));//总电压
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("total_current"),Bytes.toBytes(resBatteryDataHistory.getTotalCurrent()));//总电流
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol1"),Bytes.toBytes(resBatteryDataHistory.getVol1()));//电芯电压1
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol2"),Bytes.toBytes(resBatteryDataHistory.getVol2()));//电芯电压2
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol3"),Bytes.toBytes(resBatteryDataHistory.getVol3()));//电芯电压3
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol4"),Bytes.toBytes(resBatteryDataHistory.getVol4()));//电芯电压4
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol5"),Bytes.toBytes(resBatteryDataHistory.getVol5()));//电芯电压5
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol6"),Bytes.toBytes(resBatteryDataHistory.getVol6()));//电芯电压6
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol7"),Bytes.toBytes(resBatteryDataHistory.getVol7()));//电芯电压7
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol8"),Bytes.toBytes(resBatteryDataHistory.getVol8()));//电芯电压8
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol9"),Bytes.toBytes(resBatteryDataHistory.getVol9()));//电芯电压9
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol10"),Bytes.toBytes(resBatteryDataHistory.getVol10()));//电芯电压10
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol11"),Bytes.toBytes(resBatteryDataHistory.getVol11()));//电芯电压11
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol12"),Bytes.toBytes(resBatteryDataHistory.getVol12()));//电芯电压12
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol13"),Bytes.toBytes(resBatteryDataHistory.getVol13()));//电芯电压13
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol14"),Bytes.toBytes(resBatteryDataHistory.getVol14()));//电芯电压14
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol15"),Bytes.toBytes(resBatteryDataHistory.getVol15()));//电芯电压15
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol16"),Bytes.toBytes(resBatteryDataHistory.getVol16()));//电芯电压16
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol17"),Bytes.toBytes(resBatteryDataHistory.getVol17()));//电芯电压17
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol18"),Bytes.toBytes(resBatteryDataHistory.getVol18()));//电芯电压18
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vol19"),Bytes.toBytes(resBatteryDataHistory.getVol19()));//电芯电压19
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("temprature1"),Bytes.toBytes(resBatteryDataHistory.getTemprature1()));//温度1
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("temprature2"),Bytes.toBytes(resBatteryDataHistory.getTemprature2()));//温度2
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("charge_num"),Bytes.toBytes(resBatteryDataHistory.getChargeNum()));//充电次数
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("soc"),Bytes.toBytes(resBatteryDataHistory.getSoc()));//总电压
+        put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("create_time"),Bytes.toBytes(resBatteryDataHistory.getCreateTime()));//创建时间
+        if(resBatteryDataHistory.getCityId()!=null){
+            put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("city_id"),Bytes.toBytes(resBatteryDataHistory.getCityId()));//城市ID
+        }
+        if(resBatteryDataHistory.getVehicleId()!=null){
+            put.addColumn(Bytes.toBytes(colunm_family_baseData),Bytes.toBytes("vehicle_id"),Bytes.toBytes(resBatteryDataHistory.getVehicleId()));//车辆ID
+        }
+        put.addColumn(Bytes.toBytes(colunm_family_extraData),Bytes.toBytes("create_user"),Bytes.toBytes(resBatteryDataHistory.getCreateUser()));//创建人
+        put.addColumn(Bytes.toBytes(colunm_family_extraData),Bytes.toBytes("source"),Bytes.toBytes(resBatteryDataHistory.getSource()));//数据来源
+        put.addColumn(Bytes.toBytes(colunm_family_extraData),Bytes.toBytes("longtitude"),Bytes.toBytes(resBatteryDataHistory.getLongtitude()));//精度
+        put.addColumn(Bytes.toBytes(colunm_family_extraData),Bytes.toBytes("latitude"),Bytes.toBytes(resBatteryDataHistory.getLatitude()));//纬度
+        put.addColumn(Bytes.toBytes(colunm_family_extraData),Bytes.toBytes("remarks"),Bytes.toBytes(resBatteryDataHistory.getRemarks()));//备注
+        put.addColumn(Bytes.toBytes(colunm_family_extraData),Bytes.toBytes("test_user_name"),Bytes.toBytes(resBatteryDataHistory.getTestUserName()));//测试用户
+        return put;
+    }
 
 
    }
--- src/test/java/test/HbaseTest/HBaseBatteryManagerTest.java
+++ src/test/java/test/HbaseTest/HBaseBatteryManagerTest.java
@@ -1,9 +1,13 @@
 package test.HbaseTest;
 
 import org.apache.activemq.openwire.tool.MultiSourceGenerator;
+import org.apache.hadoop.hbase.filter.CompareFilter;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 功能：测试HBase的工具类
@@ -59,6 +63,58 @@ public class HBaseBatteryManagerTest {
         manager.addBatteryData(battery);
     }
 
+    @Test
+    public void addBatteryDataListTest() throws IOException {
+        List<ResBatteryDataHistory> batteryList=new ArrayList<>();
+        for(int i=20000;i<200000;i++){
+            ResBatteryDataHistory battery=new ResBatteryDataHistory();
+            battery.setBatteryNo(String.valueOf(i));
+            battery.setBatteryType(0);
+            battery.setVoltageDeviation(1F);
+            battery.setTotalVoltage(1F);
+            battery.setCreateTime("2016-12-12 11:11:11");
+            battery.setCreateUser(250);
+            battery.setSource(3);
+            battery.setVol1(1F);
+            battery.setVol2(1F);
+            battery.setVol3(1F);
+            battery.setVol4(1F);
+            battery.setVol5(1F);
+            battery.setVol6(1F);
+            battery.setVol7(1F);
+            battery.setVol8(1F);
+            battery.setVol9(1F);
+            battery.setVol10(1F);
+            battery.setVol11(1F);
+            battery.setVol12(1F);
+            battery.setVol13(1F);
+            battery.setVol14(1F);
+            battery.setVol15(1F);
+            battery.setVol16(1F);
+            battery.setVol17(1F);
+            battery.setVol18(1F);
+            battery.setVol19(1F);
+            battery.setTemprature1(1F);
+            battery.setTemprature2(1F);
+            battery.setChargeNum(11F);
+            battery.setLongtitude(12F);
+            battery.setLatitude(12F);
+            battery.setRemarks("lhc-代码测试");
+            battery.setTotalCurrent(22F);
+            battery.setSoc(22F);
+            battery.setTestUserName("lhc");
+            battery.setCityId(11011);
+            battery.setVehicleId(11011);
+            batteryList.add(battery);
+        }
+        long begin=System.currentTimeMillis();
+        System.out.println("开始存入："+begin);
+        manager.addBatteryDataList(batteryList);
+        long end=System.currentTimeMillis();
+        System.out.println("结束存入："+end);
+        System.out.println((end-begin)/1000.0);
+    }
+
     /**
      * 测试根据RowKey获取数据
      * @throws IOException
@@ -67,5 +123,61 @@ public class HBaseBatteryManagerTest {
     public void testGetByRowKey() throws IOException {
         System.out.println(manager.getBatteryDataHistoryByRowKey("40151060021481685540182"));
     }
+
+    /**
+     * 测试获取单元格数据
+     */
+    @Test
+    public void getCellDataTest() throws IOException {
+        System.out.println(Bytes.toFloat(manager.getCellData("40151060021481685540182",manager.colunm_family_baseData,"vol1")));
+    }
+
+
+    /**
+     * 更新用户名
+     * @throws IOException
+     */
+    @Test
+    public void updateCellTest() throws IOException {
+        System.out.println(manager.getBatteryDataHistoryByRowKey("40151060021481685540182"));
+        manager.updateCell("40151060021481685540182",manager.colunm_family_extraData,"test_user_name",Bytes.toBytes("liuhuichao128"));
+        System.out.println(manager.getBatteryDataHistoryByRowKey("40151060021481685540182"));
+    }
+
+    /**
+     * 测试表扫描
+     * @throws IOException
+     */
+    @Test
+    public void scanTest() throws IOException {
+        System.out.println( manager.getResultScans().size());
+    }
+
+
+    /**
+     * 测试and查询
+     */
+    @Test
+    public void QueryDataByConditionsAnd() throws IOException {
+        List<QueryCondition> queryConditionList=new ArrayList<>();
+        queryConditionList.add(new QueryCondition(manager.colunm_family_baseData,"vol1", CompareFilter.CompareOp.EQUAL,Bytes.toBytes(1F)));
+        long begin=System.currentTimeMillis();
+        System.out.println(manager.QueryDataByConditionsAnd(queryConditionList).size());
+        long end=System.currentTimeMillis();
+        System.out.println((end-begin)/1000.00);
+    }
+
+    /**
+     * 测试or查询
+     * @throws IOException
+     */
+    @Test
+    public void QueryDataByConditionsOrTest() throws IOException {
+        List<QueryCondition> queryConditionList=new ArrayList<>();
+        queryConditionList.add(new QueryCondition(manager.colunm_family_baseData,"vol1", CompareFilter.CompareOp.EQUAL,Bytes.toBytes(1F)));
+        System.out.println(manager.QueryDataByConditionsOr(queryConditionList));
+    }
+
+
 
 }
