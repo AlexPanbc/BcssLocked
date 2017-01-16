3c9292df8b81a7db22f95525196d0476ee100088
liuhuichao <2207221755@qq.com>
2017-01-16 10:36:01 +0800

    添加消息处理类-liuhuichao-2017年1月16日10:35:57

---
+++ src/test/java/test/kafkaTest/ConsumerReceiver.java
@@ -0,0 +1,14 @@
+package test.kafkaTest;
+
+/**
+ * 消费者
+ * Created by liuhuichao on 2017/1/16.
+ */
+public class ConsumerReceiver {
+    /**
+     * 消费方法
+     */
+    public void consume(){
+
+    }
+}
